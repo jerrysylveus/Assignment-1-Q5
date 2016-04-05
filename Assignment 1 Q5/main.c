33c5797a17f7c493520ea11e64c75ea885df4a2e
Jerry Sylveus <jerrysylveus@jerry-sylveuss-macbook.local>
2016-04-04 20:37:14 -0400

    Initial Commit

---
+++ Assignment 1 Q5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment 1 Q5
+//
+//  Created by Jerry Sylveus on 4/4/16.
+//  Copyright © 2016 Jerry Sylveus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
